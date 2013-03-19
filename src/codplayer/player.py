25dddba213c8c4970e38e16c62cf0be4e515344d
Peter Liljenberg <peter.liljenberg@gmail.com>
2013-03-19 23:37:18 +0100

    Tail audio files that are being ripped, retrying if catching up with cdrdao.

--- src/codplayer/player.py
+++ src/codplayer/player.py
@@ -11,6 +11,7 @@ The unit of time in all objects is one sample.
 """
 
 import os
+import errno
 import select
 import subprocess
 import time
@@ -107,6 +108,9 @@ class Player(object):
             if rc is not None:
                 self.debug('ripping process finished with status {0}', rc)
                 self.rip_process = None
+
+                if self.streamer:
+                    self.streamer.rip_finished()
             
         self.handle_audio()
 
@@ -145,7 +149,8 @@ class Player(object):
                 self.stopped()
 
             else:
-                open('test.cdr', 'ab').write(p.data)
+                # TODO: play for real
+                self.dest_file.write(p.data)
 
 
     def cmd_disc(self, args):
@@ -245,13 +250,15 @@ class Player(object):
 
         self.log('playing disk: {0}', disc)
 
+        self.dest_file = open('test.cdr', 'wb')
+
         # There shouldn't be a streamer, but if there is stop it
         if self.streamer:
             self.streamer.shutdown()
             self.streamer = None
 
         # Set up streaming from position 0
-        self.streamer = AudioStreamer(self, disc, 0)
+        self.streamer = AudioStreamer(self, disc, 0, self.rip_process is not None)
         self.current_disc = disc
 
         # Initialise state to new disc
@@ -275,6 +282,8 @@ class Player(object):
 
         self.write_state()
 
+        self.dest_file.close()
+        self.dest_file = None
 
     def write_state(self):
         # TODO: write to file
@@ -293,6 +302,187 @@ class Player(object):
 
     
 
+class AudioStreamer(object):
+    """Streamer for audio samples.  Encapsulates a thread reading from
+    the file (to handle any IO waits) and a queue for passing samples
+    with meta data to the player.  The player can ask the thread to
+    stop, typically used when stopping playing or skipping forward or
+    backward.
+    """
+    
+    # Special objects to signal the end of the stream
+    class STREAM_ERROR: pass
+    class END_OF_STREAM: pass
+
+    # Perhaps make this configurable sometime
+    MAX_BUFFER_SECS = 20
+    PACKETS_PER_SECOND = 5
+
+    def __init__(self, player, disc, track_number, is_ripping):
+        """Set up an audio stream from DISC, starting at TRACK_NUMBER.
+        """
+
+        self.player = player
+        self.is_ripping = is_ripping
+        self.keep_running = True
+
+        self.disc = disc
+        self.first_track_number = track_number
+
+        self.queue = Queue.Queue(self.PACKETS_PER_SECOND * self.MAX_BUFFER_SECS)
+        self.underflow_logged = False
+        
+        self.thread = threading.Thread(target = self.run_thread)
+        self.thread.daemon = True
+        self.thread.start()
+
+
+    def get_packet(self):
+        """Return the next queued audio packet, or None if the queue is empty.
+        """
+        try:
+            packet = self.queue.get_nowait()
+            self.underflow_logged = False
+            return packet
+        except Queue.Empty:
+            if not self.underflow_logged:
+                self.underflow_logged = True
+                self.player.debug('underflow getting audio from thread {0}',
+                                  self.thread.name)
+            return None
+        
+
+    def shutdown(self):
+        """Shut down the streamer thread, discarding any queued packets.
+        """
+
+        # Assume the global interpreter lock allows us to flip this flag safely
+        self.keep_running = False
+
+        # Empty queue to ensure that the thread sees the flag
+        try:
+            while True:
+                self.queue.get_nowait()
+        except Queue.Empty:
+            pass
+                
+
+    def rip_finished(self):
+        """Call to inform that the any concurrent ripping process is finished."""
+
+        # Assume the global interpreter lock allows us to flip this flag safely
+        self.is_ripping = False
+
+
+    def run_thread(self):
+        end_of_stream = self.STREAM_ERROR
+        try:
+            self.player.debug('{0}: streamer started for {1} track {2}',
+                              self.thread.name, self.disc, self.first_track_number)
+
+
+            # Construct full path to data file and open it
+
+            db_id = self.player.db.disc_to_db_id(self.disc.disc_id)
+            path = os.path.join(
+                self.player.db.get_disc_dir(db_id),
+                self.disc.data_file_name)
+                    
+            self.player.debug('{0}: opening file {1}',
+                              self.thread.name, path)
+                    
+
+            self.audio_file = None
+
+            # Retry opening file if the ripping process is in progress
+            # and might not have had time to create it yet
+
+            while self.audio_file is None:
+                try:
+                    self.audio_file = open(path, 'rb')
+                except IOError, e:
+                    if e.errno == errno.ENOENT and self.is_ripping:
+                        self.player.debug('{0}: retrying opening file {1}',
+                                          self.thread.name, path)
+                        time.sleep(0.5)
+                    else:
+                        self.player.log('{0}: error opening file {1}: {2}',
+                                        self.thread.name, path, e)
+                        return
+
+
+            # Iterate over all packets, reading data into them
+
+            for p in AudioPacket.iterate(self.disc,
+                                         self.first_track_number,
+                                         self.PACKETS_PER_SECOND):
+
+                # Very simple thread signalling...
+                if not self.keep_running:
+                    return
+
+                try:
+                    self.read_data_into_packet(p)
+                except IOError, e:
+                    self.player.log('{0}: error reading from file {1}: {2}',
+                                    self.thread.name, path, e)
+                    return
+
+                # Finally send out packet to consumer
+                self.queue.put(p)
+
+            # end of loop, handle next packet
+
+            # If we got here, then all streamed fine
+            end_of_stream = self.END_OF_STREAM
+
+        finally:
+            self.queue.put(end_of_stream)
+            self.player.debug('{0}: streamer shutting down on {1}',
+                              self.thread.name, end_of_stream.__name__)
+            self.player = None
+
+
+    def read_data_into_packet(self, p):
+        """Thread helper method for populating data into packet P."""
+
+        length = p.length * self.disc.sample_format.sample_bytes
+
+        if p.file_pos is None:
+            # Silence, so send on null bytes to player
+            p.data = '\0' * length
+
+        else:
+            file_pos = p.file_pos * self.disc.sample_format.sample_bytes
+            
+            self.audio_file.seek(file_pos)
+
+            p.data = self.audio_file.read(length)
+            length -= len(p.data)
+            file_pos += len(p.data)
+            
+            # If we didn't get all data, iterate with a timeout until
+            # it's all been read or the ripping process has stopped.
+            # This is not very efficient, and there's a small race
+            # condition at the end of the disc, but this should be
+            # very rare so keep it unoptimised for now.
+
+            while length > 0 and self.is_ripping:
+                time.sleep(1)
+                
+                self.audio_file.seek(file_pos)
+                d = self.audio_file.read(length)
+                length -= len(d)
+                file_pos += len(d)
+
+                p.data += d
+                        
+            # Still didn't get all data, treat it as an exception
+            if length > 0:
+                raise IOError('unexpected end of file, expected at least {0} bytes'
+                              .format(length))
+        
+            
 class AudioPacket(object):
     """A packet of audio data coming from a single track and index.
 
@@ -401,138 +591,7 @@ class AudioPacket(object):
                 yield p
 
                                 
-class AudioStreamer(object):
-    """Streamer for audio samples.  Encapsulates a thread reading from
-    the file (to handle any IO waits) and a queue for passing samples
-    with meta data to the player.  The player can ask the thread to
-    stop, typically used when stopping playing or skipping forward or
-    backward.
-    """
-    
-    # Special objects to signal the end of the stream
-    class STREAM_ERROR: pass
-    class END_OF_STREAM: pass
 
-    # Perhaps make this configurable sometime
-    MAX_BUFFER_SECS = 20
-    PACKETS_PER_SECOND = 5
-
-    def __init__(self, player, disc, track_number):
-        """Set up an audio stream from DISC, starting at track_number.
-        """
-
-        self.player = player
-
-        self.disc = disc
-        self.first_track_number = track_number
-
-        self.queue = Queue.Queue(self.PACKETS_PER_SECOND * self.MAX_BUFFER_SECS)
-        self.underflow_logged = False
-        
-        self.keep_running = True
-
-        self.thread = threading.Thread(target = self.run_thread)
-        self.thread.daemon = True
-        self.thread.start()
-
-
-    def get_packet(self):
-        """Return the next queued audio packet, or None if the queue is empty.
-        """
-        try:
-            packet = self.queue.get_nowait()
-            self.underflow_logged = False
-            return packet
-        except Queue.Empty:
-            if not self.underflow_logged:
-                self.underflow_logged = True
-                self.player.debug('underflow getting audio from thread {0}',
-                                  self.thread.name)
-            return None
-        
-
-    def shutdown(self):
-        """Shut down the streamer thread, discarding any queued packets.
-        """
-
-        # Assume the global interpreter lock allows us to flip this flag safely
-        self.keep_running = False
-
-        # Empty queue to ensure that the thread sees the flag
-        try:
-            while True:
-                self.queue.get_nowait()
-        except Queue.Empty:
-            pass
-                
-
-    def run_thread(self):
-        end_of_stream = self.STREAM_ERROR
-        try:
-            self.player.debug('streamer {0} started for {1} track {2}',
-                              self.thread.name, self.disc, self.first_track_number)
-
-
-            # Construct full path to data file and open it
-
-            db_id = self.player.db.disc_to_db_id(self.disc.disc_id)
-            path = os.path.join(
-                self.player.db.get_disc_dir(db_id),
-                self.disc.data_file_name)
-                    
-            self.player.debug('{0}: opening file {1}',
-                              self.thread.name, path)
-                    
-            try:
-                audio_file = open(path, 'rb')
-            except IOError, e:
-                self.player.log('{0}: error opening file {1}: {2}',
-                                self.thread.name, path, e)
-                return
-
-
-            # Iterate over all packets, reading data into them
-
-            for p in AudioPacket.iterate(self.disc,
-                                         self.first_track_number,
-                                         self.PACKETS_PER_SECOND):
-
-                # Very simple thread signalling...
-                if not self.keep_running:
-                    return
-
-                # Read from file
-                try:
-                    length = p.length * self.disc.sample_format.sample_bytes
-                    p.data = audio_file.read(length)
-                except IOError, e:
-                    self.player.log('{0}: error reading file {1}: {2}',
-                             self.thread.name, path, e)
-                    return
-                    
-                if len(p.data) < length:
-                    # TODO: handle reaching the end of a file currently being ripped
-
-                    self.player.log('{0}: unexpected end of file {1}: {2}',
-                             self.thread.name, path, e)
-                    return
-
-                # Finally send out packet to consumer
-                self.queue.put(p)
-
-            # end of loop, handle next packet
-
-            # If we got here, then all streamed fine
-            end_of_stream = self.END_OF_STREAM
-
-        finally:
-            self.queue.put(end_of_stream)
-            self.player.debug('streamer {0} shutting down on {1}',
-                              self.thread.name, end_of_stream.__name__)
-            self.player = None
-
-        
-            
 class CommandReader(object):
     """Wrapper around the file object for the command channel.
 
