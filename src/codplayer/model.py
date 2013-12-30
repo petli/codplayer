petli/codplayer

commit cca8714d924f3406342aca0666c98d5a42f48616

Restructure model into database and external views of discs and tracks

src/codplayer/model.py
@@ -54,26 +54,63 @@ class DiscInfoError(Exception):
     pass
 
     
-# Actual data classes
+#
+# Base data classes containing the basic attributes that are used both
+# in the database and in communication with external components
+#
 
 class Disc(serialize.Serializable):
-    """Represents a CD, consisting of a number of tracks.
-    """
-
     def __init__(self):
-        self.data_file_name = None
-        self.data_file_format = None
-        self.audio_format = None
-
         self.disc_id = None
         
         self.tracks = []
         
+        # Information we might get from the TOC, or failing that MusicBrainz
         self.catalog = None
-        
+        self.title = None
+        self.artist = None
+
+        # Additional information we might get from MusicBrainz (not
+        # every possible morsel, but what might be useful to keep locally)
+        self.barcode = None
+        self.release_date = None
 
     def __str__(self):
         return self.disc_id
+
+
+class Track(serialize.Serializable):
+    def __init__(self):
+        self.number = 0
+        self.length = 0
+
+        # Where index switch from 0 to 1
+        self.pregap_offset = 0
+
+        # Any additional indices
+        self.index = []
+
+        # Information we might get from the TOC, or failing that MusicBrainz
+        self.isrc = None
+        self.title = None
+        self.artist = None
+        
+
+#
+# Database versions of disc and track classes
+#
+
+class DbDisc(Disc):
+    """Represents a CD, consisting of a number of tracks.  All time
+    values are in frames.
+    """
+
+    def __init__(self):
+        super(DbDisc, self).__init__()
+        
+        self.data_file_name = None
+        self.data_file_format = None
+        self.audio_format = None
 
 
     def add_track(self, track):
@@ -83,7 +120,7 @@ class Disc(serialize.Serializable):
 
     @classmethod
     def from_toc(cls, toc, disc_id):
-        """Parse a TOC generated by cdrdao into a Disc instance.
+        """Parse a TOC generated by cdrdao into a DbDisc instance.
 
         This is not a full parse of all varieties that cdrdao itself
         allows, as this function is only intended to be used on TOCs
@@ -92,7 +129,7 @@ class Disc(serialize.Serializable):
         @param toc: a cdrdao TOC as a string
         @param disc_id: a Musicbrainz disc ID (not calculated from the TOC)
 
-        @return: A L{Disc} object.
+        @return: A L{DbDisc} object.
 
         @raise DiscInfoError: if the TOC can't be parsed.
         """
@@ -124,7 +161,7 @@ class Disc(serialize.Serializable):
                     disc.add_track(track)
 
                 if line == 'TRACK AUDIO':
-                    track = Track()
+                    track = DbTrack()
                 else:
                     # Just skip non-audio tracks
                     track = None
@@ -224,7 +261,7 @@ class Disc(serialize.Serializable):
 
     @classmethod
     def from_musicbrainz_disc(cls, mb_disc, filename = None):
-        """Translate a L{musicbrainz2.model.Disc} into a L{Disc}.
+        """Translate a L{musicbrainz2.model.Disc} into a L{DbDisc}.
         This object will contain much less information than one read
         from a TOC, but will allow a data file to be played before
         cdrdao has finished writing the TOC.
@@ -234,7 +271,7 @@ class Disc(serialize.Serializable):
         @param filename: the filename for the data file that is
         expected to be generated by cdrdao.
 
-        @return: a L{Disc} object.
+        @return: a L{DbDisc} object.
         """
 
         tracks = mb_disc.getTracks()
@@ -318,63 +355,62 @@ class Disc(serialize.Serializable):
         return self.get_disc_file_size_frames() * self.audio_format.bytes_per_frame
 
 
-class Track(serialize.Serializable):
+class DbTrack(Track):
     """Represents one track on a disc and its offsets and indices.
+    All time values are in frames.
     """
 
     def __init__(self):
-        self.number = 0
-
+        super(DbTrack, self).__init__()
+        
         self.file_offset = 0
-        self.length = 0
         self.file_length = 0
-
-        # Where index switch from 0 to 1
-        self.pregap_offset = 0
 
         # If part or all of the pregap isn't contained in the data
         # file at all
         self.pregap_silence = 0
         
-        # Any additional indices
-        self.index = []
-
-        self.isrc = None
         
 
-class ExternalDisc(serialize.Serializable):
+#
+# External views of the database objects
+#
+
+class ExtDisc(Disc):
     """External view of a Disc, hiding internal details and exposing
     all lengths as whole seconds.
     """
 
     def __init__(self, disc = None):
+        super(ExtDisc, self).__init__()
+        
         if disc:
+            assert isinstance(disc, DbDisc)
             self.disc_id = disc.disc_id
-            self.tracks = [ExternalTrack(t, disc) for t in disc.tracks]
+            self.tracks = [ExtTrack(t, disc) for t in disc.tracks]
             self.catalog = disc.catalog
-        else:
-            self.disc_id = None
-            self.tracks = []
-            self.catalog = None
+            self.title = disc.title
+            self.artist = disc.artist
+            self.barcode = disc.barcode
+            self.release_date = disc.release_date
 
 
-class ExternalTrack(serialize.Serializable):
+class ExtTrack(Track):
     """External view of a track, hiding internal details and exposing
     all lengths as whole seconds.
     """
 
     def __init__(self, track = None, disc = None):
+        super(ExtTrack, self).__init__()
+        
         if track:
+            assert isinstance(track, DbTrack)
+            assert isinstance(disc, DbDisc)
+            
             self.number = track.number
             self.length = int(track.length / disc.audio_format.rate)
             self.pregap_offset = int(track.pregap_offset / disc.audio_format.rate)
             self.index = [int(i / disc.audio_format.rate) for i in track.index]
             self.isrc = track.isrc
-        else:
-            self.number = 0
-            self.length = 0
-            self.pregap_offset = 0
-            self.index = []
-            self.isrc = None
-    
-    
+            self.title = track.title
+            self.artist = track.artist

src/codplayer/audio.py
@@ -176,9 +176,9 @@ class AudioPacket(object):
     It has the following attributes (all positions and lengths count
     audio frames, as usual):
     
-    disc: a model.Disc object 
+    disc: a model.DbDisc object 
 
-    track: a model.Track object 
+    track: a model.DbTrack object 
 
     track_number: the number of the track in the play order, counting
     from 0 (and not always equal to track.number - 1, e.g. when randomising

src/codplayer/db.py
@@ -324,7 +324,7 @@ class Database(object):
             raise DatabaseError('error reading {0}: {1}'.format(
                     orig_toc_file, e))
         
-        return model.Disc.from_toc(toc_data, self.db_to_disc_id(db_id))
+        return model.DbDisc.from_toc(toc_data, self.db_to_disc_id(db_id))
 
 
     def create_disc_dir(self, db_id):

src/codplayer/player.py
@@ -550,7 +550,7 @@ class Player(object):
         db_id = self.db.disc_to_db_id(mbd.getId())
         path = self.db.create_disc_dir(db_id)
 
-        disc = model.Disc.from_musicbrainz_disc(
+        disc = model.DbDisc.from_musicbrainz_disc(
             mbd, filename = self.db.get_audio_path(db_id))
         
         self.log('ripping new disk: {0}', disc)
@@ -712,7 +712,7 @@ class Player(object):
 
     def write_disc(self):
         if self.current_disc:
-            serialize.save_json(model.ExternalDisc(self.current_disc), self.cfg.disc_file)
+            serialize.save_json(model.ExtDisc(self.current_disc), self.cfg.disc_file)
         else:
             serialize.save_json(None, self.cfg.disc_file)
             

src/codplayer/test/test_audio.py
@@ -15,7 +15,7 @@ class TestAudioPacket(unittest.TestCase):
 
     def test_no_pregap_or_index(self):
 
-        t = model.Track()
+        t = model.DbTrack()
         t.file_offset = 5000
         t.length = 50000
 
@@ -32,7 +32,7 @@ class TestAudioPacket(unittest.TestCase):
         
     def test_pregap_and_index(self):
 
-        t = model.Track()
+        t = model.DbTrack()
         t.file_offset = 5000
         t.length = 50000
         t.pregap_offset = 3000
@@ -73,7 +73,7 @@ class TestAudioPacket(unittest.TestCase):
         
     def test_silent_pregap(self):
 
-        t = model.Track()
+        t = model.DbTrack()
         t.file_offset = 5000
         t.length = 50000
         t.pregap_offset = 3000
@@ -126,7 +126,7 @@ TWO_CHANNEL_AUDIO
 FILE "data.cdr" 00:02:00 00:00:17
 '''
         
-        disc = model.Disc.from_toc(toc, 'testId')
+        disc = model.DbDisc.from_toc(toc, 'testId')
 
         # Work with three packets per second, i.e. 25 frames per
         # packet, to make the test case reasonably easy to write

src/codplayer/test/test_model.py
@@ -24,7 +24,7 @@ class TestDiscFromToc(unittest.TestCase):
 CD_DA
 '''
         with self.assertRaises(model.DiscInfoError):
-            model.Disc.from_toc(toc, 'testId')
+            model.DbDisc.from_toc(toc, 'testId')
 
 
     def test_catalog_and_basic_track(self):
@@ -41,7 +41,7 @@ TRACK AUDIO
 TWO_CHANNEL_AUDIO
 FILE "data.cdr" 0 02:54:53
 '''
-        d = model.Disc.from_toc(toc, 'testId')
+        d = model.DbDisc.from_toc(toc, 'testId')
 
         self.assertEqual(d.disc_id, 'testId')
 
@@ -82,7 +82,7 @@ NO PRE_EMPHASIS
 TWO_CHANNEL_AUDIO
 FILE "data.cdr" 06:24:43 03:36:67
 '''
-        d = model.Disc.from_toc(toc, 'testId')
+        d = model.DbDisc.from_toc(toc, 'testId')
 
         self.assertEqual(d.catalog, None)
         self.assertEqual(d.data_file_name, "data.cdr")
@@ -124,7 +124,7 @@ FILE "data.cdr" 0 02:54:53 // foo bar
   // TWO_CHANNEL_AUDIO
 // FILE "data.cdr" 02:54:53 03:29:65
 '''
-        d = model.Disc.from_toc(toc, 'testId')
+        d = model.DbDisc.from_toc(toc, 'testId')
 
         self.assertIsNone(d.catalog)
 
@@ -148,7 +148,7 @@ SILENCE 03:48:35
 FILE "data.cdr" 0 03:27:10
 START 03:48:35
 '''
-        d = model.Disc.from_toc(toc, 'testId')
+        d = model.DbDisc.from_toc(toc, 'testId')
 
         self.assertEqual(len(d.tracks), 1)
 
@@ -172,7 +172,7 @@ START 00:01:22
 INDEX 00:03:11
 INDEX 00:05:00
 '''
-        d = model.Disc.from_toc(toc, 'testId')
+        d = model.DbDisc.from_toc(toc, 'testId')
 
         self.assertEqual(len(d.tracks), 1)
 
@@ -210,7 +210,7 @@ CD_TEXT {
 }
 '''
         with self.assertRaises(model.DiscInfoError):
-            model.Disc.from_toc(toc, 'testId')
+            model.DbDisc.from_toc(toc, 'testId')
 
 
     def test_track_isrc(self):
@@ -220,7 +220,7 @@ TWO_CHANNEL_AUDIO
 ISRC "GBAYE0000351"
 FILE "data.cdr" 0 03:27:10
 '''
-        d = model.Disc.from_toc(toc, 'testId')
+        d = model.DbDisc.from_toc(toc, 'testId')
 
         self.assertEqual(len(d.tracks), 1)
 
@@ -249,7 +249,7 @@ class TestDiscFromMusicbrainz(unittest.TestCase):
         mb_d = MusicbrainzDiscDummy()
 
         with self.assertRaises(model.DiscInfoError):
-            model.Disc.from_musicbrainz_disc(mb_d)
+            model.DbDisc.from_musicbrainz_disc(mb_d)
 
 
     def test_tracks(self):
@@ -258,7 +258,7 @@ class TestDiscFromMusicbrainz(unittest.TestCase):
             (34780, 37470),
             (72250, 9037))
 
-        d = model.Disc.from_musicbrainz_disc(mb_d, 'test.cdr')
+        d = model.DbDisc.from_musicbrainz_disc(mb_d, 'test.cdr')
 
         self.assertEqual(d.disc_id, 'testId')
 

commit a1f6bbe340cf2306fddbf5f649b7bb308ca34a0c

Fix bug on ripping introduced in cca8714d924f3406342aca0666c98d5a42f48616

src/codplayer/model.py
@@ -304,7 +304,7 @@ class DbDisc(Disc):
         first_frame = tracks[0][0]
 
         for start, length in tracks:
-            track = Track()
+            track = DbTrack()
             track.file_offset = (start - first_frame) * PCM.audio_frames_per_cd_frame
             track.length = length * PCM.audio_frames_per_cd_frame
             track.file_length = track.length