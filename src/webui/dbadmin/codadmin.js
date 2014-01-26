32361a7d72e8700f7246b3797c2297feab5a8d87
Peter Liljenberg <peter.liljenberg@gmail.com>
2014-01-26 01:23:44 +0100

    [dbadmin] Refactor disc views to handle musicbrainz selection view, and allow to filter big list on discs with/without info

--- src/webui/dbadmin/codadmin.js
+++ src/webui/dbadmin/codadmin.js
@@ -5,6 +5,7 @@
 // Distributed under an MIT license, please see LICENSE in the top dir.
 
 $(function(){
+    'use strict';
 
     //
     // Disc model and collection
@@ -26,6 +27,18 @@ $(function(){
     var discs = new DiscList();
 
     //
+    // Musicbrainz disc info model and collection
+    //
+    
+    var MBDisc = Backbone.Model.extend({
+        idAttribute: 'mb_id',
+    });
+
+    var MBDiscList = Backbone.Collection.extend({
+        model: MBDisc,
+    });
+
+    //
     // Keep track of alerts
     //
     
@@ -39,18 +52,130 @@ $(function(){
     var currentAlert = new Alert();
     
     //
-    // Table view of a disc
+    // List view of a disc.  This consists of a master view holding
+    // the position in the list of discs, and a specialised view for
+    // the kind of information displayed.
     //
 
     var DiscRowView = Backbone.View.extend({
         tagName: 'li',
         className: 'list-group-item disc',
 
+        initialize: function() {
+            this.discView = null;
+            this.setView(new DiscOverView({ model: this.model }));
+        },
+
+        render: function() {
+            this.discView.render();
+            return this;
+        },
+
+        setView: function(view) {
+            this.discView = view;
+            this.listenTo(view, 'disc-view:overview', this.onOverView);
+            this.listenTo(view, 'disc-view:details', this.onViewDetails);
+            this.listenTo(view, 'disc-view:edit', this.onViewEdit);
+            this.listenTo(view, 'disc-view:mbinfo', this.onViewMBInfo);
+            this.el.appendChild(view.el);
+
+            if (this.model.get('artist') || this.model.get('title')) {
+                this.$el.addClass('disc-with-info');
+                this.$el.removeClass('disc-without-info');
+            }
+            else {
+                this.$el.addClass('disc-without-info');
+                this.$el.removeClass('disc-with-info');
+            }
+
+            this.render();
+        },
+
+        dropView: function() {
+            this.stopListening(this.discView);
+            this.discView.remove();
+            this.discView = null;
+        },
+
+        onOverView: function() {
+            console.log('switching to: overview');
+            this.dropView();
+            this.setView(new DiscOverView({ model: this.model }));
+        },
+
+        onViewDetails: function() {
+            console.log('switching to: details');
+            this.dropView();
+            this.setView(new DiscDetailsView({ model: this.model }));
+        },
+
+        onViewEdit: function(mbDisc) {
+            console.log('switching to: edit');
+            this.dropView();
+            this.setView(new DiscEditView({ model: this.model, mbDisc: mbDisc }));
+        },
+
+        onViewMBInfo: function(mbDiscs) {
+            console.log('switching to: info');
+            this.dropView();
+            this.setView(new DiscMBInfoView({ model: this.model, collection: mbDiscs }));
+        },
+    });
+
+    var DiscOverView = Backbone.View.extend({
+        tagName: 'div',
+
         events: {
-            'click .disc-row': 'toggleDetails',
-            'click .edit-disc': 'startEdit',
-            'click .save-edit': 'saveEdit',
-            'click .cancel-edit': 'cancelEdit',
+            'click .disc-row': 'onToggleDetails',
+        },
+
+        template: _.template($('#disc-row-template').html()),
+
+        initialize: function() {
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        onToggleDetails: function() {
+            var that = this;
+
+            if (typeof this.model.get('tracks') === 'number') {
+                // We only have the partial disc info, so fetch
+                // the full structure
+                
+                // TODO: provide some feedback to the user here
+
+                this.model.fetch({
+                    success: function() {
+                        that.trigger('disc-view:details');
+                    },
+
+                    error: function(model, response) {
+                        currentAlert.set({
+                            header: 'Error fetching disc details:',
+                            message: response.statusText + ' (' + response.status + ')',
+                        });
+                    },
+                });
+            }
+            else {
+                that.trigger('disc-view:details');
+            }
+        },
+    });
+
+
+    var DiscDetailsView = Backbone.View.extend({
+        tagName: 'div',
+
+        events: {
+            'click .toggle-details': 'onToggleDetails',
+            'click .edit-disc': 'onStartEdit',
+            'click .fetch-musicbrainz': 'onFetchMusicbrainz',
         },
 
         rowTemplate: _.template($('#disc-row-template').html()),
@@ -58,94 +183,128 @@ $(function(){
         
         template: function(obj) {
             var html = this.rowTemplate(obj);
-            if (this.showDetail) {
-                html += this.detailTemplate(obj);
-            }
+            html += this.detailTemplate(obj);
             return html;
         },
 
         initialize: function() {
             this.listenTo(this.model, 'change', this.render);
-
-            this.showDetail = false;
-            this.editing = false;
         },
 
         render: function() {
-            this.setEditing(this.editing);
             this.$el.html(this.template(this.model.toJSON()));
             return this;
         },
 
-        setEditing: function(editing) {
-            this.editing = editing;
-            if (editing) {
-                this.$('.disc-row').removeClass('hover-row');
-                this.$('.track-row').removeClass('hover-row');
-                this.$('.view-only').hide();
-                this.$('.edit-only').show();
+        onToggleDetails: function() {
+            this.trigger('disc-view:overview');
+        },
+
+        onStartEdit: function() {
+            this.trigger('disc-view:edit');
+        },
+
+        onFetchMusicbrainz: function() {
+            var mbDiscs;
+            var that = this;
+            
+            mbDiscs = new MBDiscList();
+            mbDiscs.url = this.model.url() + '/musicbrainz';
+
+            // Disable all buttons before we do anything else and
+            // provide feedback in the button
+            this.$('fieldset').prop('disabled', true);
+            this.$('.fetch-musicbrainz').button('loading');
+
+            mbDiscs.fetch({
+                success: function(collection) {
+                    that.fetchMusicbrainzSuccess(collection);
+                },
+
+                error: function(collection, response) {
+                    that.fetchMusicbrainzError(response);
+                },
+            });
+        },
+
+        fetchMusicbrainzSuccess: function(mbDiscs) {
+            this.$('.fetch-musicbrainz').button('reset');
+            this.$('fieldset').prop('disabled', false);
+
+            if (mbDiscs.length === 0) {
+                // This should be a 404, but handle it in any case
+                currentAlert.set({
+                    header: 'Sorry,',
+                    message: 'Musicbrainz has no information about this disc',
+                });
+            }
+            else if (mbDiscs.length === 1) {
+                this.trigger('disc-view:edit', mbDiscs.at(0));
             }
             else {
-                this.$('.disc-row').addClass('hover-row');
-                this.$('.track-row').addClass('hover-row');
-                this.$('.edit-only').hide();
-                this.$('.view-only').show();
+                this.trigger('disc-view:mbinfo', mbDiscs);
             }
         },
 
-        toggleDetails: function() {
-            var that = this;
+        fetchMusicbrainzError: function(response) {
+            this.$('.fetch-musicbrainz').button('reset');
+            this.$('fieldset').prop('disabled', false);
 
-            // Can't toggle while editing
-            if (this.editing) {
-                return;
-            }
-
-            if (this.showDetail) {
-                this.$('.disc-details').slideUp(function() {
-                    that.showDetail = false;
-                    that.render();
+            if (response.status === 404) {
+                currentAlert.set({
+                    header: 'Sorry,',
+                    message: 'Musicbrainz has no information about this disc',
                 });
             }
             else {
-                if (typeof this.model.get('tracks') === 'number') {
-                    // We only have the partial disc info, so fetch
-                    // the full structure
+                currentAlert.set({
+                    header: 'Error fetching info:',
+                    message: response.statusText + ' (' + response.status + ')',
+                });
+            }
+        },
+    });
 
-                    this.model.fetch({
-                        success: function() {
-                            that.showDetails();
-                        },
+    var DiscEditView = Backbone.View.extend({
+        tagName: 'div',
 
-                        error: function(model, response) {
-                            currentAlert.set({
-                                header: 'Error fetching disc details:',
-                                message: response.statusText + ' (' + response.status + ')',
-                            });
-                        },
-                    });
+        events: {
+            'click .save-edit': 'onSaveEdit',
+            'click .cancel-edit': 'onCancelEdit',
+        },
+
+        
+        template: _.template($('#disc-edit-template').html()),
+
+        initialize: function(options) {
+            var mbTracks, modelTracks;
+            
+            this.mbDisc = options.mbDisc;
+
+            if (this.mbDisc) {
+                // Ensure this has the right number of tracks
+
+                mbTracks = this.mbDisc.get('tracks');
+                modelTracks = this.model.get('tracks');
+
+                while (mbTracks.length < modelTracks.length) {
+                    mbTracks.push(_.clone(modelTracks[mbTracks.length]));
                 }
-                else {
-                    this.showDetails();
+
+                while (mbTracks.length > modelTracks.length) {
+                    mbTracks.pop();
                 }
-                
             }
         },
 
-        showDetails: function() {
-            this.showDetail = true;
-            this.render();
-            this.$('.disc-details').slideDown();
+        render: function() {
+            var model = this.mbDisc || this.model;
+
+            this.$el.html(this.template(model.toJSON()));
+            return this;
         },
 
-        startEdit: function() {
-            // Ensure the forms are enabled
-            this.$('fieldset').prop('disabled', false);
-
-            this.setEditing(true);
-        },
-
-        saveEdit: function() {
+        onSaveEdit: function() {
             // Get the values of the edit fields.  Put them all into a
             // map so we can call model.save() and have them all
             // stashed to the server atomically(ish)
@@ -187,14 +346,11 @@ $(function(){
             // from server.  Also set editing mode to false so that a
             // successful update renders the viewing mode directly.
             
-            that.model.save(save, {
+            this.model.save(save, {
                 wait: true,
-                success: function() {
-                    // Unlock fields so the Edit button is enabled again
-                    that.$('fieldset').prop('disabled', false);
 
-                    // Flip to view mode
-                    that.setEditing(false);
+                success: function() {
+                    that.trigger('disc-view:details');
                 },
 
                 error: function(model, xhr) {
@@ -210,8 +366,73 @@ $(function(){
             });
         },
 
-        cancelEdit: function() {
-            this.setEditing(false);
+        onCancelEdit: function() {
+            this.trigger('disc-view:details');
+        },
+    });
+
+    var DiscMBInfoView = Backbone.View.extend({
+        tagName: 'div',
+
+        events: {
+            'click .cancel-mbinfo': 'onCancel',
+        },
+        
+        template: _.template($('#disc-mbinfo-template').html()),
+
+        render: function() {
+            var that = this;
+            var row = null;
+            
+            this.$el.html(this.template(this.model.toJSON()));
+            this.collection.each(function(model) {
+                var view = new MBDiscView({ model: model });
+                that.listenTo(view, 'disc-view:edit', that.onSelect);
+
+                if (row === null) {
+                    row = $('<div class="row mb-disc-row">');
+                    that.$('div.mb-discs').append(row);
+                    row.append(view.render().el);
+                } else
+                {
+                    row.append(view.render().el);
+                    row = null;
+                }
+            });
+
+            return this;
+        },
+
+        onSelect: function(mbDisc) {
+            this.trigger('disc-view:edit', mbDisc);
+        },
+
+        onCancel: function() {
+            this.trigger('disc-view:details');
+        },
+    });
+
+
+    var MBDiscView = Backbone.View.extend({
+        tagName: 'div',
+        className: 'mb-disc col-xs-12 col-md-6 hover-row',
+        
+        events: {
+            'click': 'onSelect',
+        },
+        
+        template: _.template($('#mbdisc-template').html()),
+
+        initialize: function() {
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        onSelect: function() {
+            this.trigger('disc-view:edit', this.model);
         },
     });
 
@@ -223,18 +444,45 @@ $(function(){
     var DiscsView = Backbone.View.extend({
         el: $('#discs'),
 
+        events: {
+            'click #show-all-discs': 'onShowAllDiscs',
+            'click #show-discs-with-info': 'onShowDiscsWithInfo',
+            'click #show-discs-without-info': 'onShowDiscsWithoutInfo',
+        },
+
         initialize: function() {
             this.listenTo(discs, 'add', this.addOne);
             this.listenTo(discs, 'reset', this.addAll);
+            this.discList = this.$('#disc-list');
         },
 
         addOne: function(disc) {
             var view = new DiscRowView({model: disc});
-            this.$el.append(view.render().el);
+            this.discList.append(view.render().el);
         },
 
         addAll: function() {
             discs.each(this.addOne, this);
+        },
+
+        onShowAllDiscs: function() {
+            this.$('.nav .active').removeClass('active');
+            this.$('#show-all-discs').addClass('active');
+            this.discList.find('.disc').show();
+        },
+
+        onShowDiscsWithInfo: function() {
+            this.$('.nav .active').removeClass('active');
+            this.$('#show-discs-with-info').addClass('active');
+            this.discList.find('.disc-without-info').hide();
+            this.discList.find('.disc-with-info').show();
+        },
+
+        onShowDiscsWithoutInfo: function() {
+            this.$('.nav .active').removeClass('active');
+            this.$('#show-discs-without-info').addClass('active');
+            this.discList.find('.disc-with-info').hide();
+            this.discList.find('.disc-without-info').show();
         },
     });
 
